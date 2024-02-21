2f90d8559b77bc98d2a434bab5e52328bfd41672
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 20:28:38 -0500

    Create 6.1.4.Two old ladies.py

---
+++ 6.Data types/6.1 Numeric data types: int, float/6.1.4.Two old ladies.py
@@ -0,0 +1,34 @@
+# Two old ladies
+# Two old ladies walk towards each other at constant speeds V1 And V2 km/h Determine after what time (in hours) the old ladies will meet if the distance between them is S km.
+# Note. These are very fast old ladies. 
+
+# Две старушки
+# Две старушки идут навстречу друг другу с постоянными скоростями 
+# V1 и V2 км/ч. Определите, через какое время (в часах) старушки встретятся, если расстояние между ними равно S км.
+# Примечание. Это очень быстрые старушки.
+
+S = float(input())
+V1 = float(input())
+V2 = float(input())
+a = S/(V1+V2)
+print(a)
+
+
+Sample Input 1:
+60.0
+15.5
+14.5
+Sample Output 1:
+2.0
+Sample Input 2:
+100.0
+60.75
+19.25
+Sample Output 2:
+1.25
+Sample Input 3:
+90.0
+32.0
+4.0
+Sample Output 3:
+2.5
