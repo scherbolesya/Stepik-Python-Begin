037838db8f2656fd3e2b8971b9258728e9960772
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:35:36 -0500

    Create 3.2.4.Reproduction n-ok.py

---
+++ 3.exam input output data/3.2.4.Reproduction n-ok.py
@@ -0,0 +1,15 @@
+# Reproduction n-ok
+# Размножение n-ок
+
+# Примечание. Для первого теста 1+11+111=123.
+# Note. For the first test 1+11+111=123.
+
+# Sample Input 1:
+# 1
+# Sample Output 1:
+# 123
+
+n = int(input())
+a = n*10 + n
+b = n*100 + n*10 + n
+print( n + a + b)
