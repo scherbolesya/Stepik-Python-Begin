306457c8840fe50d53d4032f6ff07dd249155794
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:09:17 -0500

    Create 2.3.5.I like Python.py

---
+++ 2.Data input/2.3.Lesson topic: sep, end, variables, PEP 8/2.3.5.I like Python.py
@@ -0,0 +1,6 @@
+# Напишите программу, которая выводит на экран текст «I***like***Python» (без кавычек).
+# Write a program that displays text on the screen
+# Примечание. Используйте необязательный параметр sep.
+# Note. Use the optional sep parameter.
+
+print('I', 'like', 'Python', sep='***') # I***like***Python
