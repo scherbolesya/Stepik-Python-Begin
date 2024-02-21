e14c02b369076bf9ea869ea57f3f4719b6bd7e07
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:35:13 -0500

    Create 3.2.3.Big number.py

---
+++ 3.exam input output data/3.2.3.Big number.py
@@ -0,0 +1,16 @@
+# Big number
+# Большое число
+
+# Sample Input:
+# 9
+# 29
+# 7
+# 27
+# Sample Output:
+# 4710194409608608369201743232
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(a**b + c**d)
