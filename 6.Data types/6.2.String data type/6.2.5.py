c6a493c0cd144de2deef3e474de40679669ea229
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-21 18:40:18 -0500

    Create 6.2.5.py

---
+++ 6.Data types/6.2.String data type/6.2.5.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая выводит текст:
+# Write a program that displays text:
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+
+# Примечание. Используйте конкатенацию строк.
+# Note. Use string concatenation.
+
+
+s = '"Python is a great language!", said Fred.'
+s1 =''' "I don't ever remember having this much fun before."'''
+print(s+s1)
+
+
+Sample Input:
+Sample Output:
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
