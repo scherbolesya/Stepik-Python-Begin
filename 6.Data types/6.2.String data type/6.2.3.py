69d1b599dfb9ea0ee0ecec6634d8bc9b600faed7
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-21 18:37:49 -0500

    Create 6.2.3.py

---
+++ 6.Data types/6.2.String data type/6.2.3.py
@@ -0,0 +1,10 @@
+# Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+str1 = '1'
+str2 = str1 + '2' + str1
+str3 = str2 + '3' + str2
+str4 = str3 + '4' + str3
+print(str4)
+
+# 121312141213121
