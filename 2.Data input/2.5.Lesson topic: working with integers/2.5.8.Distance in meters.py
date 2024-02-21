fda7c6f215ee89a343835e926a6154d2edcb7383
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:32:33 -0500

    Create 2.5.8.Distance in meters.py

---
+++ 2.Data input/2.5.Lesson topic: working with integers/2.5.8.Distance in meters.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая находит полное число метров по заданному числу сантиметров.
+# Write a program that finds the total number of meters given the number of centimeters.
+
+sm = int(input())
+m = sm // 100
+print(m)
+
+# Sample Input 1:
+# 345
+# Sample Output 1:
+# 3
