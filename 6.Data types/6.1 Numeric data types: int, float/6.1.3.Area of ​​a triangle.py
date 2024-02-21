88bd0d652be3115e06675d3f7ec3a82388915404
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 20:25:31 -0500

    Create 6.1.3.Area of ​​a triangle.py

---
+++ 6.Data types/6.1 Numeric data types: int, float/6.1.3.Area of ​​a triangle.py
@@ -0,0 +1,27 @@
+# Area of ​​a triangle
+# Write a program that reads the lengths of two legs in a right triangle and prints its area.
+
+# Площадь треугольника
+# Напишите программу, которая считывает длины двух катетов в прямоугольном треугольнике и выводит его площадь.
+
+a = float(input())
+b = float(input())
+s = 0.5 * a * b 
+print(s)
+
+
+Sample Input 1:
+443
+390
+Sample Output 1:
+86385.0
+Sample Input 2:
+32.2
+25.5
+Sample Output 2:
+410.55
+Sample Input 3:
+5544.25
+6100.0
+Sample Output 3:
+16909962.5
