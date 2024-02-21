3a656d54d7081b2138fabe9b45c273a8b846256e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:37:19 -0500

    Create 4.1.10.Smaller of two numbers.py

---
+++ 4. Conditional operator/4.1.Choice of two/4.1.10.Smaller of two numbers.py
@@ -0,0 +1,20 @@
+# Smaller of two numbers
+# Наименьшее из двух чисел
+
+a = int(input())
+b = int(input())
+if a > b:
+    print(b)
+else:
+    print(a)
+
+# Sample Input 1:
+# 8
+# 11
+# Sample Output 1:
+# 8
+# Sample Input 2:
+# 20
+# 5
+# Sample Output 2:
+# 5
