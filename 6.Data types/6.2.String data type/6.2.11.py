6075e766ce94597b98c6cae408051ae6b7fed56a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-21 18:50:25 -0500

    Create 6.2.11.py

---
+++ 6.Data types/6.2.String data type/6.2.11.py
@@ -0,0 +1,15 @@
+# Какие значения может принимать строковая переменная s, чтобы в результате выполнения кода было выведено слово «YES»?
+# What values can the string variable s take so that the word “YES” is printed as a result of executing the code?
+
+
+if s in 'abc123abc':
+    print('YES')
+else:
+    print('NO')
+
+
+s = '23'
+s = '1'
+s = '123abc'
+s = 'a'
+s = '3ab'
