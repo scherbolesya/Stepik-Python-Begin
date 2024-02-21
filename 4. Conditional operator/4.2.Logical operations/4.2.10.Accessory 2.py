4e0f2fb4c11acdaca5bcc00ad8c3c7e25ac18908
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:42:00 -0500

    Create 4.2.10.Accessory 2.py

---
+++ 4. Conditional operator/4.2.Logical operations/4.2.10.Accessory 2.py
@@ -0,0 +1,23 @@
+# Accessory 2
+# Принадлежность 2
+# Write a program that accepts an integer x and determines whether it does not belong to the range from -3 to 7.
+# Напишите программу, которая принимает целое число x и определяет, не принадлежит промежутку от -3 до 7.
+
+x = int(input())
+if - 3 < x < 7:
+    print('Не принадлежит')
+else:
+    print('Принадлежит')
+
+# Sample Input 1:
+# -44
+# Sample Output 1:
+# Принадлежит
+# Sample Input 2:
+# -3
+# Sample Output 2:
+# Принадлежит
+# Sample Input 3:
+# 5
+# Sample Output 3:
+# Не принадлежит
