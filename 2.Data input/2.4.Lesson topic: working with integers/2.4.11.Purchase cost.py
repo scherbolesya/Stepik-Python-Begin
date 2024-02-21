80e4a08308ae3ba4dc63d1925fda38c652633e94
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:11:59 -0500

    Create 2.4.11.Purchase cost.py

---
+++ 2.Data input/2.4.Lesson topic: working with integers/2.4.11.Purchase cost.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая считает стоимость трех компьютеров, состоящих из монитора, системного блока, клавиатуры и мыши.
+# Write a program that calculates the cost of three computers, consisting of a monitor, a system unit, a keyboard and a mouse.
+
+# Sample Input 3:
+# 44990
+# 123300
+# 19600
+# 8990
+# Sample Output 3:
+# 590640 
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = (a + b + c + d)*3
+print (e)
