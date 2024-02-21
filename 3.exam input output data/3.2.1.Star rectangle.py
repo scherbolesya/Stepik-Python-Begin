0b8d2d30ce0278bdc82bb1c51d909ed471a20203
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:34:23 -0500

    Create 3.2.1.Star rectangle.py

---
+++ 3.exam input output data/3.2.1.Star rectangle.py
@@ -0,0 +1,22 @@
+# Звездный прямоугольник
+
+# Напишите программу, которая выводит прямоугольник, по периметру состоящий из звездочек (*).
+
+# Star rectangle
+
+# Write a program that prints a rectangle with asterisks (*) around its perimeter.
+
+print(
+"""
+*****************
+*               *
+*               *
+*****************
+""")
+
+# Sample Input:
+# Sample Output:
+# *****************
+# *               *
+# *               *
+# *****************
