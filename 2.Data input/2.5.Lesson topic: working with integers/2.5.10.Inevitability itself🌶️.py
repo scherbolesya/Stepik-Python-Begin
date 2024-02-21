08882ca42016f5c622ec75d0044d12ad583bfdb2
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:14:24 -0500

    Create 2.5.10.Inevitability itself🌶️.py

---
+++ 2.Data input/2.5.Lesson topic: working with integers/2.5.10.Inevitability itself🌶️.py
@@ -0,0 +1,18 @@
+# Сама неотвратимость 🌶️
+
+# Безумный титан Танос собрал все 6 камней бесконечности и намеревается уничтожить половину населения Вселенной по щелчку пальцев. 
+# При этом если население Вселенной является нечетным числом, то титан проявит милосердие и округлит количество выживших в большую сторону. 
+# Помогите Мстителям подсчитать количество выживших.
+
+# The mad titan Thanos has collected all 6 infinity stones and intends to destroy half the population of the universe with the snap of his fingers. 
+# Moreover, if the population of the Universe is an odd number, then the titan will show mercy and round up the number of survivors. 
+# Help the Avengers count the number of survivors.
+
+n = int(input())
+v = n // -2
+print(v*(-1))
+
+# Sample Input 1:
+# 99
+# Sample Output 1:
+# 50
