214d5fcc9e1e977e97cd618a7b2610de7066d86d
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 20:48:13 -0500

    Create 6.1.11.py

---
+++ 6.Data types/6.1 Numeric data types: int, float/6.1.11.py
@@ -0,0 +1,5 @@
+# What number will be displayed on the screen as a result of executing the following code?
+# Какое число будет выведено на экран в результате выполнения следующего кода?
+num = max(1, 3, -5, 7) + min(-3, 6, -8, -1) + abs(-17)
+print(num)
+# 16
