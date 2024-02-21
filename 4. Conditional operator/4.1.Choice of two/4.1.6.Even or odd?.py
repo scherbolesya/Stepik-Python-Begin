139c856a297e37447683a4f32850cf92d2723711
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-20 19:39:55 -0500

    Create 4.1.6.Even or odd?.py

---
+++ 4. Conditional operator/4.1.Choice of two/4.1.6.Even or odd?.py
@@ -0,0 +1,24 @@
+# Even or odd?
+# Write a program that determines whether a number is even or odd.
+
+# Четное или нечетное?
+# Напишите программу, которая определяет, является число четным или нечетным.
+
+a = int(input())
+if a % 2 == 0:
+    print('Четное')
+else:
+    print('Нечетное')
+  
+# Sample Input 1:
+# 10
+# Sample Output 1:
+# Четное
+# Sample Input 2:
+# 17
+# Sample Output 2:
+# Нечетное
+# Sample Input 3:
+# 0
+# Sample Output 3:
+# Четное
