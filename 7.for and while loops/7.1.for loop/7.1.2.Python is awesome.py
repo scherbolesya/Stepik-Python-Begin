5b20de80f620ee0746d0df4d0035863d9e7c58e8
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-22 19:42:29 -0500

    Create 7.1.2.Python is awesome.py

---
+++ 7.for and while loops/7.1.for loop/7.1.2.Python is awesome.py
@@ -0,0 +1,18 @@
+# Write a program that prints the words "Python is awesome!" (without quotes) 10 times.
+# Напишите программу, которая выводит слова «Python is awesome!» (без кавычек) 10 раз.
+
+for i in range(10):
+    print('Python is awesome!')
+
+Sample Input:
+Sample Output:
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
