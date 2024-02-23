b34af9793b720acb3ef1205ca874bca387e25315
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-22 19:46:25 -0500

    Create 7.1.4.Character sequence.py

---
+++ 7.for and while loops/7.1.for loop/7.1.4.Character sequence.py
@@ -0,0 +1,36 @@
+# Character sequence
+# Последовательность символов
+# Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+# Write a program that uses exactly three for loops to print the following sequence of characters:
+AAA
+AAA
+AAA
+AAA
+AAA
+AAA
+BBBB
+BBBB
+BBBB
+BBBB
+BBBB
+E
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+G
+
+
+for i in range(6):
+    print('AAA')
+for i in range(5):
+    print('BBBB')
+print('E')
+for i in range(9):
+    print('TTTTT')
+print('G')
