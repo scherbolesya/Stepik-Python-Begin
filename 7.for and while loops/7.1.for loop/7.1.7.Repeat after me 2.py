833e59e63009a810a54a0b3b2557ead52115ca73
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-22 19:56:59 -0500

    Create 7.1.7.Repeat after me 2.py

---
+++ 7.for and while loops/7.1.for loop/7.1.7.Repeat after me 2.py
@@ -0,0 +1,37 @@
+# Repeat after me 2
+# Повторяй за мной 2
+# Напишите программу, которая считывает одну строку текста и выводит 10 строк, пронумерованных от 0 до 9, каждая с указанной строкой текста.
+# Write a program that reads one line of text and outputs 10 lines, numbered 0 to 9, each with the specified line of text.
+
+
+a = input()
+for i in range(10):
+    print(i, a)
+  
+
+Sample Input 1:
+LeBron
+Sample Output 1:
+0 LeBron
+1 LeBron
+2 LeBron
+3 LeBron
+4 LeBron
+5 LeBron
+6 LeBron
+7 LeBron
+8 LeBron
+9 LeBron
+Sample Input 2:
+Roman
+Sample Output 2:
+0 Roman
+1 Roman
+2 Roman
+3 Roman
+4 Roman
+5 Roman
+6 Roman
+7 Roman
+8 Roman
+9 Roman
