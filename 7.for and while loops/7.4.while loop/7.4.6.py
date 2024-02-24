7f7785356e8a90ebd07cd3124534cb7cb323e72b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-23 21:37:53 -0500

    Create 7.4.6.py

---
+++ 7.for and while loops/7.4.while loop/7.4.6.py
@@ -0,0 +1,7 @@
+Какое число нужно написать вместо многоточия, чтобы цикл выполнился ровно 7 раз?
+# What number should be written instead of the ellipsis so that the loop is executed exactly 7 times?
+i = 5
+while i <= ...:
+    print('Python awesome!')
+    i += 1
+# 11
