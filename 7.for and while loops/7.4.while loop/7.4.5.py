a720dfdb87727aa5e1c8b240906d0704c04f5beb
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-23 21:36:42 -0500

    Create 7.4.5.py

---
+++ 7.for and while loops/7.4.while loop/7.4.5.py
@@ -0,0 +1,6 @@
+Сколько раз сообщение «Python awesome!» будет напечатано в приведенном ниже фрагменте кода?
+# How many times does the message “Python awesome!” will be printed in the code snippet below?
+count = 1
+while count < 10:
+    print('Python awesome!')
+# бесконечно много раз
