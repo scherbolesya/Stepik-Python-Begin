4612ac7ffc317d220a215c188920104a7f5ae161
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-23 22:00:27 -0500

    Create 7.5.3.py

---
+++ 7.for and while loops/7.5.While loop: processing the digits of a number/7.5.3.py
@@ -0,0 +1,12 @@
+Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+num = 123456789
+total = 0
+while num != 0:
+    last_digit = num % 10
+    if last_digit > 4:
+        total += 1
+    num = num // 10
+print(total)
+
+# 5
