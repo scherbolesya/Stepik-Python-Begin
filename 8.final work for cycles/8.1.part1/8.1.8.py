79b1b6303d983f098cf5957d05736184b09b836c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:45:12 -0500

    Create 8.1.8.py

---
+++ 8.final work for cycles/8.1.part1/8.1.8.py
@@ -0,0 +1,9 @@
+# What number should be written instead of the ellipsis so that the loop is executed exactly 7 times?
+Какое число нужно написать вместо многоточия, чтобы цикл выполнился ровно 7 раз?
+
+i = ...
+while i <= 10:
+    print ('Python!')
+    i += 1
+
+# 4
