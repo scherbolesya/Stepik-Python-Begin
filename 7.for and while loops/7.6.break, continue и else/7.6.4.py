c326a51d7cfb8d370e8b6b0d98ffaeb345a689d9
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:14:49 -0500

    Create 7.6.4.py

---
+++ 7.for and while loops/7.6.break, continue и else/7.6.4.py
@@ -0,0 +1,10 @@
+# What will the code snippet below show?
+Что покажет приведенный ниже фрагмент кода?
+
+n = 10
+while n > 0:
+    n -= 1
+    if n == 2:
+        continue
+    print(n, end='*')
+# 9*8*7*6*5*4*3*1*0*
