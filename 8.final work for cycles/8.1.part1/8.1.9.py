af673726a0d0743fa5e215857a000ad7f67c7194
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:45:58 -0500

    Create 8.1.9.py

---
+++ 8.final work for cycles/8.1.part1/8.1.9.py
@@ -0,0 +1,12 @@
+# Determine what problem the following code fragment solves:
+Определите какую задачу решает следующий фрагмент кода:
+
+n = int(input())
+res = 1
+i = 2
+while i <= n:
+    res *= i
+    i += 1
+print(res)
+
+# выводит факториал числа n
