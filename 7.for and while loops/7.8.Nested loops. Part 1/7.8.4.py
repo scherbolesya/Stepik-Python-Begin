07f90fc1663795ce951b46281dd9f4167493ab79
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:58:30 -0500

    Create 7.8.4.py

---
+++ 7.for and while loops/7.8.Nested loops. Part 1/7.8.4.py
@@ -0,0 +1,12 @@
+# What will the code snippet below show?
+Что покажет приведенный ниже фрагмент кода?
+
+counter = 0
+for i in range(99, 102):
+    temp = i
+    while temp > 0:
+        counter += 1
+        temp //= 10
+print(counter)
+
+# 8
