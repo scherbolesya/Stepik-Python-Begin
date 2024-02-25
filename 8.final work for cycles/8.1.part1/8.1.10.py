bcb6095e3a7728a60135d931bf3cd856b0044495
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:46:57 -0500

    Create 8.1.10.py

---
+++ 8.final work for cycles/8.1.part1/8.1.10.py
@@ -0,0 +1,10 @@
+# Determine what problem the following code fragment solves:
+Определите какую задачу решает следующий фрагмент кода:
+
+n = int(input())
+i = 2
+while n % i != 0:
+    i += 1
+print(i)
+
+# выводит минимальный делитель числа, отличный от единицы
