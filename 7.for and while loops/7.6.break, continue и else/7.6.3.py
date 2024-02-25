6dd7cd94e2bcd1a44a14cdcdba65667ce2d22f9b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:13:57 -0500

    Create 7.6.3.py

---
+++ 7.for and while loops/7.6.break, continue и else/7.6.3.py
@@ -0,0 +1,10 @@
+# What will the code snippet below show?
+Что покажет приведенный ниже фрагмент кода?
+
+i = 100
+while i > 0:
+    if i == 40:
+        break
+    print(i, end='*')
+    i -= 20
+# 100*80*60*
