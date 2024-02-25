33bc14dee022430a788568378b57ae38b1df7d23
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:17:38 -0500

    Create 7.8.12.An ancient problem.py

---
+++ 7.for and while loops/7.8.Nested loops. Part 1/7.8.12.An ancient problem.py
@@ -0,0 +1,12 @@
+# An ancient problem
+# There are 100 rubles. How many bulls, cows and calves can be bought with all this money, if the price for a bull is 10 rubles, for a cow - 5 rubles,
+# for a calf - 0.5 rubles and you need to buy 100 heads of cattle?
+# Note. Use a nested for loop.
+Старинная задача
+Имеется 100 рублей. Сколько быков, коров и телят можно купить на все эти деньги, если плата за быка – 10 рублей, за корову – 5 рублей, 
+за теленка – 0.5 рубля и надо купить 100 голов скота?
+Примечание. Используйте вложенный цикл for.
+
+Количество быков:  1
+Количество коров:  9
+Количество телят:  90
