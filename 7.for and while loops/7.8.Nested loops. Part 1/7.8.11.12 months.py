19088843a19be85391142f4ca9000ece6a1a7c91
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:15:54 -0500

    Create 7.8.11.12 months.py

---
+++ 7.for and while loops/7.8.Nested loops. Part 1/7.8.11.12 months.py
@@ -0,0 +1,13 @@
+# 12 months
+# Solve the equation in natural numbers 28n+30k+31m=365.
+# Note. Use a nested for loop. Write down the solution with the smallest value of n first.
+12 месяцев
+Решите уравнение в натуральных числах 28n+30k+31m=365.
+Примечание. Используйте вложенный цикл for. В первую очередь запишите решение с наименьшим значением n.
+n =  1
+k =  4
+m =  7
+или
+n =  2
+k =  1
+m =  9                                
