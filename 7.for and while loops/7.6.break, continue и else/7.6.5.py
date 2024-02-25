9d9b04fdebdd1507a18d4fdfe33ca762af978387
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:15:36 -0500

    Create 7.6.5.py

---
+++ 7.for and while loops/7.6.break, continue и else/7.6.5.py
@@ -0,0 +1,10 @@
+# What will the code snippet below show?
+Что покажет приведенный ниже фрагмент кода?
+
+result = 0
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    result += i
+print(result)
+# 25
