2c291416f2ca0bd1149fd30295c080be50522610
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:56:37 -0500

    Create 7.8.2.py

---
+++ 7.for and while loops/7.8.Nested loops. Part 1/7.8.2.py
@@ -0,0 +1,16 @@
+# Set in what order the specified nested loop will output pairs of numbers (i, j).
+Установите в каком порядке, указанный вложенный цикл выведет пары чисел (i, j). 
+
+for i in range(1, 4):
+    for j in range(3, 6):
+        print(i, j)
+
+1 3
+1 4
+1 5
+2 3
+2 4
+2 5
+3 3
+3 4
+3 5
