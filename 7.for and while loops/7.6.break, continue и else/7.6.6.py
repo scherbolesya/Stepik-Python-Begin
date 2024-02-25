c19141b9faaa1f92d843e52e92eedabc940e3f1a
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:16:19 -0500

    Create 7.6.6.py

---
+++ 7.for and while loops/7.6.break, continue и else/7.6.6.py
@@ -0,0 +1,13 @@
+# What will the code snippet below show?
+ Что покажет приведенный ниже фрагмент кода?
+
+mult = 1
+for i in range(1, 11):
+   if i % 2 == 0:
+      continue
+   if i % 9 == 0:
+      break
+   mult *= i
+print(mult)
+
+# 105
