413e69b2d3a35de7c4d43c4d1d9a8371af27fe75
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 20:24:54 -0500

    Create 7.6.10.py

---
+++ 7.for and while loops/7.6.break, continue и else/7.6.10.py
@@ -0,0 +1,11 @@
+# Will the else code block in the code snippet below be executed?
+Будет ли выполнен блок кода else, в приведенном ниже фрагменте кода?
+
+n = 0
+while n < 10:
+    n += 2
+    print(n)
+else:
+    print('Цикл завершен.')
+
+# Да
