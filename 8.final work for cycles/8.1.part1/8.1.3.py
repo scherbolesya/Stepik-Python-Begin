bcf0cc877021cbaf45520061c5249fb99bf588a7
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-02-24 21:41:11 -0500

    Create 8.1.3.py

---
+++ 8.final work for cycles/8.1.part1/8.1.3.py
@@ -0,0 +1,3 @@
+# What sequence of numbers will the function call range(0, 501, 100) give you?
+Какую последовательность чисел даст вам вызов функции range(0, 501, 100)?
+# 0, 100, 200, 300, 400, 500
