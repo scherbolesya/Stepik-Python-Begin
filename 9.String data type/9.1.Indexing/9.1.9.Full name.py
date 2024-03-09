28377a50d45e9b3b6d7117bd1061c85c75b47c80
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 20:18:00 -0500

    Create 9.1.9.Full name.py

---
+++ 9.String data type/9.1.Indexing/9.1.9.Full name.py
@@ -0,0 +1,17 @@
+ФИО
+# Full name
+На вход программе подаются три строки: имя, фамилия и отчество (именно в таком порядке). Напишите программу, которая выводит инициалы человека.
+# The program receives three lines as input: first name, last name and patronymic (in that order). Write a program that prints a person's initials.
+
+i = input() #Программа должна вывести ФИО человека.
+f = input()
+o = input()
+print(f[0]+i[0]+o[0])
+
+
+Sample Input:
+Тимур
+Гуев
+Ахсарбекович
+Sample Output:
+ГТА
