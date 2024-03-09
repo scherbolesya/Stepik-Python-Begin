d3b80202c2a40e43d82a51775cf222fb5ca52f05
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:21:07 -0500

    Create 10.1.3.py

---
+++ 10.final work on lines/10.1.part1/10.1.3.py
@@ -0,0 +1,5 @@
+Если попытаться использовать индекс, который находится за пределами диапазона строкового значения, то
+# If you try to use an index that is outside the range of the string value, then
+
+# произойдет ошибка IndexError
+# an IndexError will occur
