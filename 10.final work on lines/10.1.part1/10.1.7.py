742e230128a51375304cf0a3de661eb2211da162
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:23:35 -0500

    Create 10.1.7.py

---
+++ 10.final work on lines/10.1.part1/10.1.7.py
@@ -0,0 +1,4 @@
+Какой строковый метод возвращает копию строкового значения, в котором удалены все ведущие и замыкающие пробельные символы?
+# Which string method returns a copy of a string value with all leading and trailing whitespace characters removed?
+
+# strip()
