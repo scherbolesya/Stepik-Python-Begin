0d17fb9332c37def3ee452167987fc0cde83ac02
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:29:29 -0500

    Create 10.2.6.py

---
+++ 10.final work on lines/10.2.part2/10.2.6.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы была выведена строка s в которой символ «o» заменен на символ «@».
+# Complete the code below so that the output of the program is the string s in which the “o” character is replaced by the “@” character.
+
+s = 'Python rocks!'
+print(s.replace('o', '@'))
