5df46605f1af18fba8fc8426c0e8d2ec4fc5d4af
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:45:03 -0500

    Create 11.1.2.py

---
+++ 11.Lists/11.1.Introduction to Lists/11.1.2.py
@@ -0,0 +1,5 @@
+Значения в списках, заключенные в квадратные скобки и отделенные запятыми, называются
+# Values in lists enclosed in square brackets and separated by commas are called
+
+# элементами
+# elements
