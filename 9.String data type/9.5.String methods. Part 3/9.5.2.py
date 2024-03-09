b1c84f3e8bd56753de228df67ab3bc5f835b48be
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 17:55:58 -0500

    Create 9.5.2.py

---
+++ 9.String data type/9.5.String methods. Part 3/9.5.2.py
@@ -0,0 +1,11 @@
+Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+s = 'aabbAA111ccDDaa'
+print(s.isalnum())
+print(s.isalpha())
+print(s.isdigit())
+
+# True
+# False
+# False
