5b3c4280526093e946f9f7ee60fb760df00019e6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 17:34:32 -0500

    Create 9.4.6.py

---
+++ 9.String data type/9.4.String methods. Part 2/9.4.6.py
@@ -0,0 +1,7 @@
+Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+s = '     I learn Python language               '
+print(s.strip())
+
+# I learn Python language
