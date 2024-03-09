f62b2fc513c68941e2347946939d839df8d3fd60
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:24:08 -0500

    Create 10.1.8.py

---
+++ 10.final work on lines/10.1.part1/10.1.8.py
@@ -0,0 +1,4 @@
+Какой строковый метод возвращает истину, если строковое значение содержит только буквы и имеет по крайней мере один символ?
+Which string method returns true if the string value contains only letters and has at least one character?
+
+# isalpha()
