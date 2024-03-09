87b112c4567f6f10e8991954723faaff863a7137
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:28:28 -0500

    Create 10.2.4.py

---
+++ 10.final work on lines/10.2.part2/10.2.4.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы была выведена строка s без ведущих и замыкающих пробельных символов.
+# Complete the code below so that the program outputs the string s without leading or trailing whitespace characters.
+
+s = '    Python rocks!     '
+print(s.strip())
