48bef365dc760500fa0de102d6b3d033ac9c5a36
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:48:13 -0500

    Create 11.1.6.py

---
+++ 11.Lists/11.1.Introduction to Lists/11.1.6.py
@@ -0,0 +1,7 @@
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+
+numbers = [0, 1, 3, 14, 2, 7, 9, 8, 10]
+print(numbers)
+
+# [0, 1, 3, 14, 2, 7, 9, 8, 10]
