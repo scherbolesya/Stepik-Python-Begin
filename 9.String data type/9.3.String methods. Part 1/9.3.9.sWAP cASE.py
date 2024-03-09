93461e5479a1dde44942fe28d0b2b1439bc9a8af
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 17:08:17 -0500

    Create 9.3.9.sWAP cASE.py

---
+++ 9.String data type/9.3.String methods. Part 1/9.3.9.sWAP cASE.py
@@ -0,0 +1,21 @@
+sWAP cASE
+На вход программе подается строка. Напишите программу, которая меняет регистр символов, другими словами замените все строчные символы заглавными и наоборот.
+# A string is given as input to the program. Write a program that changes the case of characters, 
+# in other words, replace all lowercase characters with uppercase ones and vice versa.
+
+s=input()
+print(s.swapcase())
+
+
+Sample Input 1:
+Swap Case
+Sample Output 1:
+sWAP cASE
+Sample Input 2:
+Www.sTepik.com
+Sample Output 2:
+wWW.StEPIK.COM
+Sample Input 3:
+Pythonist 2
+Sample Output 3:
+pYTHONIST 2
