0d2830f0481dbe3560e5d377d3555b3b18aa1c02
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:49:17 -0500

    Create 11.1.7.py

---
+++ 11.Lists/11.1.Introduction to Lists/11.1.7.py
@@ -0,0 +1,7 @@
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+
+names = ['Michael', 'John', 'Freddie']
+print(names)
+
+# ['Michael', 'John', 'Freddie']
