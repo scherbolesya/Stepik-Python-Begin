2bc3a0c609dd8e2cb4e859c017da3f99c46b2cec
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 20:13:50 -0500

    Create 9.1.6.py

---
+++ 9.String data type/9.1.Indexing/9.1.6.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя индексатор, так чтобы он вывел символ w.
+# Complete the given code using the indexer so that it prints the character w.
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[39])
