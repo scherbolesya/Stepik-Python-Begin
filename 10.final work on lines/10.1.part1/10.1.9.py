2b4ce1c7100ede4605810ca5d6eae44a7e785cbd
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:24:46 -0500

    Create 10.1.9.py

---
+++ 10.final work on lines/10.1.part1/10.1.9.py
@@ -0,0 +1,4 @@
+Какой строковый метод возвращает истину, если строковое значение содержит только цифры и имеет по крайней мере один символ?
+# Which string method returns true if the string value contains only numbers and has at least one character?
+
+# isdigit()
