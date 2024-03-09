750056a6cd3100edd5ce3b35de2d23ad0e635a5c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:26:33 -0500

    Create 10.2.1.py

---
+++ 10.final work on lines/10.2.part2/10.2.1.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы была выведена длина строки s.
+# Complete the code below so that the program outputs the length of the string s.
+
+s = 'Python rocks!'
+print(len(s))
