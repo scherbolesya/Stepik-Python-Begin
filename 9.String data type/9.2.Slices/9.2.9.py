0f219057c65de8120f80e712e89e4df670a85ea9
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 21:26:00 -0500

    Create 9.2.9.py

---
+++ 9.String data type/9.2.Slices/9.2.9.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя срезы, так чтобы он вывел каждый 7 символ строки s (начиная с 0-го индекса).
+# Complete the above code using slices so that it prints every 7th character of string s (starting at index 0).
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::7])
