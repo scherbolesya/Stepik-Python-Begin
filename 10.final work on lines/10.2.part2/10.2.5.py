89d886838f86d31e6e3fdd1a34d296222448d75b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:28:58 -0500

    Create 10.2.5.py

---
+++ 10.final work on lines/10.2.part2/10.2.5.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы была выведена строка s заглавными буквами (в верхнем регистре).
+# Complete the code below so that the program outputs the string s in capital letters (uppercase).
+
+s = 'Python rocks!'
+print(s.upper())
