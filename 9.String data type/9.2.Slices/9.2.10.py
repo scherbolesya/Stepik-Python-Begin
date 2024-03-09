e7626e5148132d7b2ff71dbc2b294f5f85369413
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 21:26:42 -0500

    Create 9.2.10.py

---
+++ 9.String data type/9.2.Slices/9.2.10.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя срезы, так чтобы он вывел строку s в обратном порядке.
+# Complete the above code using slices so that it prints the string s in reverse order.
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[::-1])
