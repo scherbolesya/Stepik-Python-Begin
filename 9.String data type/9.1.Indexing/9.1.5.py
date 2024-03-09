52408f9056eb86c3e47ffd31074c73549cbb7e90
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 20:12:51 -0500

    Create 9.1.5.py

---
+++ 9.String data type/9.1.Indexing/9.1.5.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя индексатор, так чтобы он вывел символ запятой.
+# Complete the following code using the indexer so that it outputs the comma character.
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print (s[7])
