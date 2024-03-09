c05b0e0bff8c176c69f9928ad7445d05f38425a6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:22:19 -0500

    Create 10.1.5.py

---
+++ 10.final work on lines/10.1.part1/10.1.5.py
@@ -0,0 +1,4 @@
+Какой строковый метод возвращает индекс первого вхождения подстроки в строку?
+# Which string method returns the index of the first occurrence of a substring in a string?
+
+# find()
