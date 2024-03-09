3b18072da565dc98affba6ede9b30d2c711dd427
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:27:47 -0500

    Create 10.2.3.py

---
+++ 10.final work on lines/10.2.part2/10.2.3.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы были выведены символы строки s со 2-го по 5-й включительно.
+# Complete the code below so that the program outputs the 2nd through 5th characters of string s.
+
+s = 'Python rocks!'
+print(s[1:5])
