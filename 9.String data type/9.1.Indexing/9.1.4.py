d02a232a792c03f7b6c3abf94a0e71249e257762
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 20:12:12 -0500

    Create 9.1.4.py

---
+++ 9.String data type/9.1.Indexing/9.1.4.py
@@ -0,0 +1,8 @@
+Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+s = '01234567891011121314151617'
+for i in range(0, len(s), 5):
+    print(s[i], end='')
+
+# 051217
