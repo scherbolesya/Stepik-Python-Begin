e17ad8e426402701f5058bea038623f01deb6793
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:02:36 -0500

    Create 9.5.8.py

---
+++ 9.String data type/9.5.String methods. Part 3/9.5.8.py
@@ -0,0 +1,9 @@
+Дополните приведенный код, используя форматирование строк с помощью f-строк, так чтобы он вывел текст: 
+«In 2010, someone paid 10K Bitcoin for two pizzas.» (без кавычек).
+# Complete the given code using string formatting with f-strings so that it outputs the text:
+# “In 2010, someone paid 10K Bitcoin for two pizzas.” (without quotes).
+
+year = 2010
+amount = '10K'
+currency = 'Bitcoin'
+print(f'In {year}, someone paid {amount} {currency} for two pizzas.')
