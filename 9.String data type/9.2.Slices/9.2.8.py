132d8f1ceb10b264b0522b0c45261bc5787d4e04
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-08 21:25:00 -0500

    Create 9.2.8.py

---
+++ 9.String data type/9.2.Slices/9.2.8.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя срезы, так чтобы он вывел последние 9 символов строки s.
+# Complete the given code using slices so that it prints the last 9 characters of the string s.
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+print(s[40:])
