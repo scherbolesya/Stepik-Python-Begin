dc1f2b01a84716db7adb1f586df2f4636d4dca7c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:27:06 -0500

    Create 10.2.2.py

---
+++ 10.final work on lines/10.2.part2/10.2.2.py
@@ -0,0 +1,5 @@
+Дополните приведенный ниже код, так чтобы в результате выполнения программы был выведен четвертый символ строки s.
+# Complete the code below so that the program outputs the fourth character of the string, s.
+
+s = 'Python rocks!'
+print(s[3:4])
