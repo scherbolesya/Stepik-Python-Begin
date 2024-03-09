05d1c77ef77e899f4c6bd0cafb7b75da7aeea4da
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 18:23:00 -0500

    Create 10.1.6.py

---
+++ 10.final work on lines/10.1.part1/10.1.6.py
@@ -0,0 +1,4 @@
+Какой строковый метод возвращает копию строкового значения, в котором удалены все ведущие пробельные символы?
+# Which string method returns a copy of a string value with all leading whitespace characters removed?
+
+# lstrip()
