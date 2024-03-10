fef8658fa45bc6f7eb29b2846f75176bab7b3b31
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:22:36 -0500

    Create 11.3.3.py

---
+++ 11.Lists/11.3.List methods. Part 1./11.3.3.py
@@ -0,0 +1,9 @@
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+
+numbers = [4, 2, 8, 6, 5]
+numbers.append(7)
+numbers.append(1)
+print(numbers)
+
+# [4, 2, 8, 6, 5, 7, 1]
