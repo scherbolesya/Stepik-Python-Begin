0aade2e8c62b4805b09d6b33b09ae9eed8c6253b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 15:52:39 -0400

    Create 11.6.4.py

---
+++ 11.Lists/11.6.List methods. Part 2/11.6.4.py
@@ -0,0 +1,9 @@
+Что будет выведено в результате выполнения следующего программного кода?
+# What will be the output as a result of executing the following code?
+
+colors = ['Red', 'Blue', 'Green', 'Black', 'White']
+del colors[-1]
+colors.remove('Green')
+print(colors)
+
+# ['Red', 'Blue', 'Black']
