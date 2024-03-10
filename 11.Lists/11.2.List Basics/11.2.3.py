4ade20cd8e1211dc8ca6b4ddb1a7acdfaa5996f6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:08:52 -0500

    Create 11.2.3.py

---
+++ 11.Lists/11.2.List Basics/11.2.3.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, используя индексатор, так чтобы он вывел последний элемент списка primes.
+# Complete the following code using an indexer so that it displays the last element of the primes list.
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+print(primes[-1])
