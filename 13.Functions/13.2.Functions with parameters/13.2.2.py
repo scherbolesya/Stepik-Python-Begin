53379447bd3dc8ade683bc3585a315a675aacd50
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:46:34 -0400

    Create 13.2.2.py

---
+++ 13.Functions/13.2.Functions with parameters/13.2.2.py
@@ -0,0 +1,5 @@
+Порция данных, которая отправляется в функцию в момент ее вызова, называется
+# The piece of data that is sent to a function at the time it is called is called
+
+# аргумент
+# argument
