586b41f0a1cdcb15acfa4ea636502a8a0223090d
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 17:38:59 -0400

    Create 12.1.6.py

---
+++ 12.final work on lists/12.1.part1/12.1.6.py
@@ -0,0 +1,9 @@
+Что покажет приведенный ниже фрагмент кода?
+# What will the code snippet below show?
+
+numbers1 = [1, 2, 3]
+numbers2 = [10, 20, 30]
+numbers3 = numbers1 + numbers2
+print(numbers3)
+
+# [1, 2, 3, 10, 20, 30]
