40255ca2b2c8a3ec8940a231bb40973d883dcd5e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:49:08 -0400

    Create 13.2.6.py

---
+++ 13.Functions/13.2.Functions with parameters/13.2.6.py
@@ -0,0 +1,17 @@
+Что покажет приведенная ниже программа?
+# What will the program below show?
+
+def change_us(a, b):
+    a = 0
+    b = 0
+    print(a, b)
+
+x = 1
+y = 7
+print(x, y)
+change_us(x, y)
+print(x, y)
+
+# 1 7
+# 0 0
+# 1 7
