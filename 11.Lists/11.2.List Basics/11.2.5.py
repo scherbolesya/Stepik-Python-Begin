e3927012b56fb420b0ae8fe13fa37d5b60d23ac4
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:11:00 -0500

    Create 11.2.5.py

---
+++ 11.Lists/11.2.List Basics/11.2.5.py
@@ -0,0 +1,5 @@
+Дополните приведенный код, так чтобы он вывел сумму минимального и максимального элементов списка numbers.
+# Complete the code below so that it prints the sum of the minimum and maximum elements of the numbers list.
+
+numbers = [12.5, 3.1415, 2.718, 9.8, 1.414, 1.1618, 1.324]
+print(min(numbers)+max(numbers))
