11308d7ec9b19903ead991a77054de2bdd4fda0f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:13:42 -0500

    Create 11.2.7.py

---
+++ 11.Lists/11.2.List Basics/11.2.7.py
@@ -0,0 +1,9 @@
+Дополните приведенный код так, чтобы элемент списка имеющий значение Green заменился на значение Зеленый, а элемент Violet на Фиолетовый. 
+Далее необходимо вывести полученный список.
+# Complete the code below so that the list element with the Green value is replaced by the Green value, and the Violet element is replaced by Violet.
+# Next you need to display the resulting list.
+
+rainbow = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+rainbow[3:4] = ['Зеленый']
+rainbow[6:7] = ['Фиолетовый']
+print(rainbow)
