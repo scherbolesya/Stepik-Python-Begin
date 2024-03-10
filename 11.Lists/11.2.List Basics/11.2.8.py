39a07067b0a541621f2198cf79f3f67471ced6e2
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:14:31 -0500

    Create 11.2.8.py

---
+++ 11.Lists/11.2.List Basics/11.2.8.py
@@ -0,0 +1,7 @@
+Дополните приведенный код так, чтобы он вывел "перевёрнутый" список languages (т.е. элементы будут идти в обратном порядке).
+Примечание. Для вывода списка воспользуйтесь функцией print().
+# Complete the above code so that it displays a "reversed" list of languages (that is, the elements will be in reverse order).
+# Note. To print a list, use the print() function.
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+print(languages[::-1])
