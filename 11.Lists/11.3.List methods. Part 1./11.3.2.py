e99d51fb17eba2a35345615b37620797bf9feb6f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:21:55 -0500

    Create 11.3.2.py

---
+++ 11.Lists/11.3.List methods. Part 1./11.3.2.py
@@ -0,0 +1,8 @@
+Допустим, программа состоит из строки кода:
+# Let's say the program consists of a line of code:
+names = []
+
+Какую из приведенных ниже инструкций следует применить для добавления в список по 0 индексу строкового значения 'Chromatica'?
+# Which of the following instructions should be used to add the string value 'Chromatica' to the list at index 0?
+
+# names.append('Chromatica')
