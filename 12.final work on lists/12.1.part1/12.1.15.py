0daf6b1351472386ca05e36bc1e664a3dc8af5c6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 17:44:44 -0400

    Create 12.1.15.py

---
+++ 12.final work on lists/12.1.part1/12.1.15.py
@@ -0,0 +1,2 @@
+В результате применения инструкции del  значение в заданной индексной позиции удаляется из списка.
+# The del statement removes the value at the specified index position from the list.
