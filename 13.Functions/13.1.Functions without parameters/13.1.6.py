92da2185b478e23a360ddfce4800e98709656431
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:08:29 -0400

    Create 13.1.6.py

---
+++ 13.Functions/13.1.Functions without parameters/13.1.6.py
@@ -0,0 +1,8 @@
+Какие из имен допустимы для названия функции в Python?
+# Which names are valid for a function name in Python?
+
+# is_valid
+# print_numbers
+# _myfunction
+# draw_triangle
+# find_sum_1
