ddd96a7dccd8e8869e47b9b3f0121e18f39f646e
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:02:47 -0400

    Create 13.3.6.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.6.py
@@ -0,0 +1,19 @@
+Что покажет приведенная ниже программа?
+# What will the program below show?
+
+def print_paris(): 
+    s = 'I love Paris'
+    print(s) 
+
+def print_london():
+    s = 'I love London'
+    print(s) 
+
+s = 'I love Moscow'
+print_paris()
+print_london()
+print(s)
+
+# I love Paris
+# I love London
+# I love Moscow
