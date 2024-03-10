6eabf3eb6712376d18a839ac37c067207e6eb535
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 15:51:56 -0400

    Create 11.6.3.py

---
+++ 11.Lists/11.6.List methods. Part 2/11.6.3.py
@@ -0,0 +1,12 @@
+Что будет выведено в результате выполнения следующего программного кода?
+# What will be the output as a result of executing the following code?
+
+colors = ['Orange']
+colors.append('Red')
+colors.append('Blue')
+colors.append('Green')
+colors.insert(0, 'Violet')
+colors.insert(2, 'Purple')
+print(colors)
+
+# ['Violet', 'Orange', 'Purple', 'Red', 'Blue', 'Green']
