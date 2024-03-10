047c56e69aeb5d4baad69f78f07352dbcc313e8b
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:23:12 -0500

    Create 11.3.4.py

---
+++ 11.Lists/11.3.List methods. Part 1./11.3.4.py
@@ -0,0 +1,9 @@
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+
+numbers = [4, 2]
+numbers.extend([1, 2, 3])
+numbers.extend([7, 17, 777])
+print(numbers)
+
+# [4, 2, 1, 2, 3, 7, 17, 777]
