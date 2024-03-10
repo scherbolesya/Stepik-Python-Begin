9004b82ba195e7246916b6f2d53cbdc8f4dd65cd
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:47:13 -0400

    Create 13.2.3.py

---
+++ 13.Functions/13.2.Functions with parameters/13.2.3.py
@@ -0,0 +1,5 @@
+Особая переменная, которая получает порцию данных, когда вызывается функция называется
+A special variable that receives a piece of data when a function is called is called
+
+# параметр
+# parameter
