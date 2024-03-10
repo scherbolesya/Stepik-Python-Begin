adad371d90be7c9901f9c5445479ad21a6a41c85
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:49:45 -0400

    Create 13.2.7.py

---
+++ 13.Functions/13.2.Functions with parameters/13.2.7.py
@@ -0,0 +1,10 @@
+Что покажет приведенная ниже программа?
+# What will the program below show?
+
+def print_text(text, num):
+    while num > 0:
+        print(text, end='')
+        num -= 1
+print_text('Python', 4)
+
+# PythonPythonPythonPython
