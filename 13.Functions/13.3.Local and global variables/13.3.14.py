f978dee9335987f7df017b96c4bbf9e44116f0f4
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:12:55 -0400

    Create 13.3.14.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.14.py
@@ -0,0 +1,12 @@
+Выберите верные утверждения.
+# Choose the correct statements.
+
+# после выхода из функции локальные переменные станут недоступны
+# локальные переменные, определенные в пределах функции, действуют только в ней
+# функция вызывается по своему имени с указанием необходимых аргументов
+# глобальные переменные доступны для всех функций
+
+# after exiting the function, local variables will no longer be available
+# local variables defined within a function have effect only within that function
+# the function is called by its name with the required arguments
+# global variables are available for all functions
