c58b0173d0554b048e7ca773b72e4af94a9e49a4
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:02:04 -0400

    Create 13.3.5.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.5.py
@@ -0,0 +1,5 @@
+Разрешается ли, чтобы локальная переменная в одной функции имела то же имя, что и локальная переменная в другой функции?
+# Is it allowed for a local variable in one function to have the same name as a local variable in another function?
+
+# да
+# yes
