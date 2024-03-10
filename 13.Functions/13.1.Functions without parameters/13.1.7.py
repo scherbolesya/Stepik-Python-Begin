98cd0e55ed595a97d3c6c589ebd7820017f6ebf9
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:09:56 -0400

    Create 13.1.7.py

---
+++ 13.Functions/13.1.Functions without parameters/13.1.7.py
@@ -0,0 +1,9 @@
+Для чего используется оператор pass?
+# What is the pass operator used for?
+В ходе исполнения оператора pass ничего не происходит, поэтому он может использоваться в качестве заглушки в тех местах, где это синтаксически необходимо, 
+например, в инструкциях, где тело является обязательным, таких как def. Таким образом, оператор pass используется там, где код пока ещё не появился, но планируется.
+# Nothing happens while the pass statement is executed, so it can be used as a placeholder in places where it is syntactically necessary.
+# for example, in statements where the body is mandatory, such as def. Thus, the pass operator is used where the code has not yet appeared, but is planned.
+
+# для создания заглушки
+# to create a stub
