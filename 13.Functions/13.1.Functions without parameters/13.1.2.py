2b8bbc7d63308379548aa834b627956a19486073
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 18:05:17 -0400

    Create 13.1.2.py

---
+++ 13.Functions/13.1.Functions without parameters/13.1.2.py
@@ -0,0 +1,5 @@
+Отдельная функционально независимая часть программы, решающая определенную задачу, называется
+# A separate functionally independent part of a program that solves a specific problem is called
+
+# функцией
+# function
