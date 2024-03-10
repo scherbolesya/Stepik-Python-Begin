44883669e243f58fb6d9040fc033640c9ea6bd96
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:09:12 -0400

    Create 13.3.10.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.10.py
@@ -0,0 +1,5 @@
+Переменная, которая видима любой функции в программном файле, называется
+# A variable that is visible to any function in a program file is called
+
+# глобальной переменной
+# global variable
