a6d5bf1f4993e4eff9c375085f7022dc3b2e6f5c
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:23:49 -0500

    Create 11.3.5.py

---
+++ 11.Lists/11.3.List methods. Part 1./11.3.5.py
@@ -0,0 +1,9 @@
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'magenta']
+del colors[2]
+del colors[6]
+print(colors)
+
+# ['red', 'orange', 'green', 'blue', 'purple', 'brown']
