89872c1478f753577f18fcffae66df553f87530f
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-09 19:09:43 -0500

    Create 11.2.4.py

---
+++ 11.Lists/11.2.List Basics/11.2.4.py
@@ -0,0 +1,7 @@
+Дополните приведенный код, используя срезы, так чтобы он вывел первые 6 элементов списка primes.
+Примечание. Результатом вывода должна быть строка [2, 3, 5, 7, 11, 13].
+# Complete the above code using slices so that it prints the first 6 elements of the primes list.
+# Note. The output should be the string [2, 3, 5, 7, 11, 13].
+
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+print(primes[0:6])
