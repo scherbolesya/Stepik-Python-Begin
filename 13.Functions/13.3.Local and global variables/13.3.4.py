28f7ef3d3b8aebbe224c3b7b0511caaf508d65f6
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:01:30 -0400

    Create 13.3.4.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.4.py
@@ -0,0 +1,9 @@
+Что покажет приведенная ниже программа?
+# What will the program below show?
+
+def print_paris(): 
+    print(s) 
+    s = 'I love Paris'
+print_paris()
+
+# UnboundLocalError: local variable 's' referenced before assignment
