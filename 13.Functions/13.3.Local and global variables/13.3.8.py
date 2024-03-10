244735561601ed00dc01442df9e9dab9daefb8b9
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-10 19:04:34 -0400

    Create 13.3.8.py

---
+++ 13.Functions/13.3.Local and global variables/13.3.8.py
@@ -0,0 +1,23 @@
+Какие из переменных в приведенном ниже коде являются локальными?
+# Which of the variables in the code below are local?
+
+number = 101
+def is_prime(num):
+    flag = True
+    for i in range(2, num):
+        if num % i == 0:
+            flag = False
+            break
+    if num != 1 and flag == True:
+        print('Число', num, 'простое.')
+    else:
+        print('Число', num, 'составное.')
+x = 17
+y = int(input())
+is_prime(x)
+is_prime(y)
+is_prime(number)
+
+# num
+# flag
+# i
