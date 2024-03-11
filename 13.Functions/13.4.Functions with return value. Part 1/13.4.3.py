dc7346005bbc1f3c9a287acd96a61f457135bdb0
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-11 15:33:23 -0400

    Create 13.4.3.py

---
+++ 13.Functions/13.4.Functions with return value. Part 1/13.4.3.py
@@ -0,0 +1,11 @@
+Функция do_something() определена следующим образом:
+# The do_something() function is defined as follows:
+
+def do_something(number):
+    return number * 2
+  
+Что будет выведено в результате выполнения следующего программного кода? 
+# Что будет выведено в результате выполнения следующего программного кода? 
+print(do_something(10))
+
+# 20
