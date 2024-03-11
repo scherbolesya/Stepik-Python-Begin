b601bad54865cca238ca463d6aa810143b82a084
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-11 15:34:33 -0400

    Create 13.4.4.py

---
+++ 13.Functions/13.4.Functions with return value. Part 1/13.4.4.py
@@ -0,0 +1,14 @@
+Функция do_something() определена следующим образом:
+# The do_something() function is defined as follows:
+
+def do_something(numbers):
+    result = 1
+    for i in numbers:
+        result *= i    
+    return result
+  
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+print(do_something([2, 2, 2, 2]))
+
+# 16
