0dd93dd923331fde316d1c95686f6635eba74ac2
Olesya <77589637+scherbolesya@users.noreply.github.com>
2024-03-11 15:35:32 -0400

    Create 13.4.5.py

---
+++ 13.Functions/13.4.Functions with return value. Part 1/13.4.5.py
@@ -0,0 +1,12 @@
+Функция get_sum() определена следующим образом:
+# The get_sum() function is defined as follows:
+
+def get_sum(x, y, z):
+    return x + y + z
+    print('Сумма равна', x + y + z)
+  
+Что будет выведено в результате выполнения следующего программного кода? 
+# What will be the output as a result of executing the following code?
+print(get_sum(1, 2, 3))
+
+# 6
